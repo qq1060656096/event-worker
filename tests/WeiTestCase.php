1a2bd15106f5e465c836cc8cb509f0a78039bec7
qq1060655096 <1060656096@qq.com>
2017-08-17 00:46:56 +0800

    aaa: 1. 增加EventLog单元测试 2. 增加EventModule单元测试 3. 增加获取Yml配置单元测试

---
+++ tests/WeiTestCase.php
@@ -0,0 +1,11 @@
+<?php
+namespace Wei\EventWork\Tests;
+
+/**
+ * 单元测测试基类
+ * Class WeiTestCase
+ * @package Wei\BriefDB\Tests
+ */
+abstract class WeiTestCase extends \PHPUnit_Framework_TestCase
+{
+}
